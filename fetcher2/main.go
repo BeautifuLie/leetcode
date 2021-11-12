628763024a6df9caf732ccbdf69250002e34e6b1
Denys <89598618+BeautifuLie@users.noreply.github.com>
2021-11-12 11:52:46 +0200

    new implement web-crawl

---
+++ fetcher2/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var mu sync.RWMutex
+var visitedMap = make(map[string]bool)
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+
+func Crawl(url string, depth int, fetcher Fetcher) {
+	mu.Lock()
+	_, ok := visitedMap[url]
+	mu.Unlock()
+
+	if ok {
+		return
+	} else {
+		mu.Lock()
+		visitedMap[url] = true
+		mu.Unlock()
+	}
+
+	if depth <= 0 {
+		return
+	}
+
+	body, urls, err := fetcher.Fetch(url)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("found: %s %q\n", url, body)
+
+	for _, u := range urls {
+
+		go Crawl(u, depth-1, fetcher)
+
+	}
+
+	return
+}
+
+func main() {
+
+	Crawl("https://golang.org/", 4, fetcher)
+
+	limitTime := time.After(2 * time.Second)
+	<-limitTime
+}
+
+// fakeFetcher is Fetcher that returns canned results.
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFetcher{
+	"https://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"https://golang.org/pkg/",
+			"https://golang.org/cmd/",
+		},
+	},
+	"https://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/cmd/",
+			"https://golang.org/pkg/fmt/",
+			"https://golang.org/pkg/os/",
+		},
+	},
+	"https://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+	"https://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+}
